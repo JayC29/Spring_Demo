a143acb5a4a21dc0cd37f097f3f5c1017780e0ab
JayC29 <jescacique@hotmail.com>
2018-09-30 19:17:42 -0500

    initial

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,69 @@
+package com.example.demo;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@SpringBootApplication
+public class DemoApplication {
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(EmployeeRepository repository) {
+		return (args) -> {
+			// save a couple of customers
+			/*
+			repository.save(new Employee("Jack", "Bauer"));
+			repository.save(new Employee("Chloe", "O'Brian"));
+			repository.save(new Employee("Kim", "Bauer"));
+			repository.save(new Employee("David", "Palmer"));
+			repository.save(new Employee("Michelle", "Dessler"));
+*/
+			// fetch all customers
+			log.info("Employees found with findAll():");
+			log.info("-------------------------------");
+			for (Employee employee : repository.findAll()) {
+				System.out.println("First Name: " + employee.getFirst_Name() + "\nLast Name: " + employee.getLast_Name() + "\nEmail: " + employee.getEmail_Address() + "\nPhone Number: " + employee.getPhone_Number()
+					+ "\nCurrent Job Title: " + employee.getCurrent_Job_Title() + "\nYears Experience: " +employee.getYears_Experience() + "\nManager Name: " + employee.getManaager_Name() + "\nHashed Password "
+						+ employee.getHashed_PW() + "\n\n");
+			}
+			log.info("");
+
+			/*// fetch an individual customer by ID
+			repository.findById(1L)
+					.ifPresent(customer -> {
+						log.info("Customer found with findById(1L):");
+						log.info("--------------------------------");
+						log.info(customer.toString());
+						log.info("");
+					});
+
+			// fetch customers by last name
+			log.info("Customer found with findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			repository.findByLastName("Bauer").forEach(bauer -> {
+				log.info(bauer.toString());
+			});
+			// for (Customer bauer : repository.findByLastName("Bauer")) {
+			// 	log.info(bauer.toString());
+			// }
+			log.info("");*/
+		};
+	}
+}
+
+
---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,113 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long   Employee_ID;
+    private int    Years_Experience;
+    private String First_Name;
+    private String Last_Name;
+    private String Email_Address;
+    private String Phone_Number;
+
+
+
+    private String Current_Job_Title;
+    private String Manaager_Name;
+    private String Hashed_PW;
+
+    protected Employee() {}
+
+    public Employee(String First_Name, String Last_Name, String Email_Address, String Phone_Number, String Current_Job_Title
+                        , int Years_Experience, String Manager_Name, String Hashed_PW) {
+
+        this.First_Name        = First_Name;
+        this.Last_Name         = Last_Name;
+        this.Email_Address     = Email_Address;
+        this.Phone_Number      = Phone_Number;
+        this.Current_Job_Title = Current_Job_Title;
+        this.Years_Experience  = Years_Experience;
+        this.Manaager_Name     = Manager_Name;
+        this.Hashed_PW         = Hashed_PW;
+
+    }
+
+    public Long getEmployee_ID() {
+        return this.Employee_ID;
+    }
+
+    public void setEmployee_ID(Long employee_ID) {
+        this.Employee_ID = employee_ID;
+    }
+
+    public int getYears_Experience() {
+        return this.Years_Experience;
+    }
+
+    public void setYears_Experience(int years_Experience) {
+        this.Years_Experience = years_Experience;
+    }
+
+    public String getFirst_Name() {
+        return this.First_Name;
+    }
+
+    public void setFirst_Name(String first_Name) {
+        this.First_Name = first_Name;
+    }
+
+    public String getLast_Name() {
+        return this.Last_Name;
+    }
+
+    public void setLast_Name(String last_Name) {
+        this.Last_Name = last_Name;
+    }
+
+    public String getEmail_Address() {
+        return this.Email_Address;
+    }
+
+    public void setEmail_Address(String email_Address) {
+        this.Email_Address = email_Address;
+    }
+
+    public String getPhone_Number() {
+        return this.Phone_Number;
+    }
+
+    public void setPhone_Number(String phone_Number) {
+        this.Phone_Number = phone_Number;
+    }
+
+    public String getCurrent_Job_Title() {
+        return this.Current_Job_Title;
+    }
+
+    public void setCurrent_Job_Title(String current_Job_Title) {
+        this.Current_Job_Title = current_Job_Title;
+    }
+
+    public String getManaager_Name() {
+        return this.Manaager_Name;
+    }
+
+    public void setManaager_Name(String manaager_Name) {
+        this.Manaager_Name = manaager_Name;
+    }
+
+    public String getHashed_PW() {
+        return this.Hashed_PW;
+    }
+
+    public void setHashed_PW(String hashed_PW) {
+        this.Hashed_PW = hashed_PW;
+    }
+}
