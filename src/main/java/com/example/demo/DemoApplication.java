8c81ecb3933fe529a60934822d241a6b820dc529
JayC29 <jescacique@hotmail.com>
2018-09-30 20:36:03 -0500

    fix search for employee table over Employees

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -29,10 +29,10 @@ public class DemoApplication {
 			// fetch all employees
 			log.info("Employees found with findAll():");
 			log.info("-------------------------------");
-			for (Employee employee : repository.findAll()) {
-				System.out.println("First Name: " + employee.getFirst_Name() + "\nLast Name: " + employee.getlastName() + "\nEmail: " + employee.getEmail_Address() + "\nPhone Number: " + employee.getPhone_Number()
-					+ "\nCurrent Job Title: " + employee.getCurrent_Job_Title() + "\nYears Experience: " +employee.getYears_Experience() + "\nManager Name: " + employee.getManager_Name() + "\nHashed Password "
-						+ employee.getHashed_PW() + "\n\n");
+			for (Employee Employees : repository.findAll()) {
+				System.out.println("First Name: " + Employees.getFirst_Name() + "\nLast Name: " + Employees.getlastName() + "\nEmail: " + Employees.getEmail_Address() + "\nPhone Number: " + Employees.getPhone_Number()
+					+ "\nCurrent Job Title: " + Employees.getCurrent_Job_Title() + "\nYears Experience: " +Employees.getYears_Experience() + "\nManager Name: " + Employees.getManager_Name() + "\nHashed Password "
+						+ Employees.getHashed_PW() + "\n\n");
 			}
 			log.info("");
 		};
